759dd062a095f29e3a25375aa399de2dc5e4fd53
JavierRojasC <62660573+JavierRojasC@users.noreply.github.com>
2021-05-31 17:56:53 -0500

    upd exmp

--- R/Exampl.R
+++ R/Exampl.R
@@ -1,13 +1,3 @@
-\name{aovby}
-\alias{aovby}
-\title{Analysis of variance bayesian with shiny panel}
-\usage{
-aovby()
-}
-\description{
-aovby()
-}
-\examples{
 RemocionPorc <- c(47.533,45.500,22.500,24.200,12.411,12.411,18.467,19.125,63.600,71.033,36.000,36.583)
 CONC.ppm <- c(3,3,6,6,9,9,12,12,3,3,12,12)
 
@@ -15,4 +5,5 @@ Data <- data.frame(CONC.ppm=as.factor(CONC.ppm),RemocionPorc)
 
 library(AovBY)
 aovbay(Data)
-}
+
+
