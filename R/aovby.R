5b2e29fd3d5053ca10554dcfc1bca992d9493885
JavierRojasC <62660573+JavierRojasC@users.noreply.github.com>
2021-05-31 11:40:31 -0500

    upd

--- R/aovby.R
+++ R/aovby.R
@@ -32,35 +32,9 @@ aovbay <- function(dataset=FALSE) {
   # )
   # Sys.setenv(R_MAKEVARS_USER = "/read-only/path/to/Makevars")
 
-  Model <- "data {
-          int<lower=0> N;
-          int<lower=0> J;
-          int<lower=1,upper=J> predictor[N];
-          vector[N] response;
-        }
-        parameters {
-          vector[J] eta;
-          real mu;
-          real<lower=0> sigmaalpha;
-          real<lower=0> sigmaepsilon;
-        }
-        transformed parameters {
-          vector[J] a;
-          vector[N] yhat;
-
-          a = mu + sigmaalpha * eta;
-
-          for (i in 1:N)
-            yhat[i] = a[predictor[i]];
-        }
-        model {
-          eta ~ normal(0, 1);
-
-          response ~ normal(yhat, sigmaepsilon);
-        }"
 
   #rt <- stanc(file='https://raw.githubusercontent.com/JavierRojasC/JavierRCam/master/oneway.stan')
-  rt <- (stanc(model_code = Model))
+  rt <- (stanc(model_code = stanmodels$modeloneway))
   sm <- stan_model(stanc_ret = rt, verbose=FALSE)
 
   left_footer <- fluidRow(
